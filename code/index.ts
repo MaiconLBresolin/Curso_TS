c5dc4e5d07a955fdacc95db800e131e6dab772a1
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-05-19 17:24:36 -0300

    feat: first commit, exercicio 1

---
+++ code/index.ts
@@ -0,0 +1,8 @@
+/* 
+//Exercicio 1
+
+let pi = "3.14159" //declared as a string, it can be resolved removing the quotation marks or simply by using the method number that converts a string into a number variable
+let tau = Number(pi) * 2
+
+console.log(`${tau} is ${pi} times two.`)
+*/
---
+++ prod/index.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+//Exercicio 1
+
+let pi = "3.14159" //declared as a string, it can be resolved removing the quotation marks or simply by using the method number that converts a string into a number variable
+let tau = Number(pi) * 2
+
+console.log(`${tau} is ${pi} times two.`)
+*/
+//Exercicio 2
+let pie; //the "problem" should be resolved just by specifying the type of data that the let pie should receive, making it impossible to assign a number to it"
+pie = "blueberry";
+console.log(`i like to eat ${pie}-flavored pie.`);
